79350d3afb0a27d5b01057d3c169949036783d77
Genotypek <39954242+Genotypek@users.noreply.github.com>
2018-09-29 13:41:26 +0200

    Dzienniczek ucznia, utworzona klasa dzienniczka [Dodawanie ocen, Obliczanie średniej, Zwracanie najwyższej i najniższe oceny]

---
+++ DzienniczekUcznia/Dzienniczek.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DzienniczekUcznia
+{
+    class Dzienniczek
+    {
+        //Stan (zmienne - POLA)
+        List<float> oceny;
+
+        //Zachowania
+        /// <summary>
+        /// Dodawanie nowych ocen do dzienniczna
+        /// </summary>
+        /// <param name="ocena"></param>
+        public void DodajOcene(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        /// <summary>
+        /// Obliczanie średniej wszystkich ocen w dzienniczku
+        /// </summary>
+        /// <returns></returns>
+        public float ObliczanieSredniej()
+        {
+            //float sum = 0, avg = 0;
+
+            //foreach (var ocena in oceny)
+            //{
+            //    sum += ocena;
+            //}
+
+            //avg = sum / oceny.Count();
+
+            return oceny.Average();
+        }
+
+        /// <summary>
+        /// Zwracanie najwyższej oceny z dzienniczka
+        /// </summary>
+        /// <returns></returns>
+        public float NajwyzszaOcena()
+        {
+            return oceny.Max();
+        }
+
+        /// <summary>
+        /// Zaracanie najniższej oceny z dzienniczka
+        /// </summary>
+        /// <returns></returns>
+        public float NajnizszaOcena()
+        {
+            return oceny.Min();
+        }
+    }
+}
